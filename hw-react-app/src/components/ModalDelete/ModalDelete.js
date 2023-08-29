94c3306e450738f61b0a0b72e17c1bcf845de7cd
Oksana Moroz <k.moroz.mail@gmail.com>
2023-08-29 19:28:16 +0300

    add modal window

---
+++ hw-react-app/src/components/ModalDelete/ModalDelete.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./ModalDelete.css";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import Button from "@mui/material/Button";
+
+const ModalDelete = ({ isOpen, isClose, isDelete }) => {
+  return (
+    <Dialog open={isOpen} onClose={isClose} className="modal-custom">
+      <DialogContent className="modal-content">
+        <DialogContentText className="modal-question">
+          Are u sure you want to delete this product?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions className="modal-buttons">
+        <Button onClick={isClose} className="button-cansel">
+          Cancel
+        </Button>
+        <Button
+          onClick={isDelete}
+          variant="contained"
+          color="secondary"
+          className="button-delete"
+        >
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ModalDelete;
